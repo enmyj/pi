bedfb2db3fa38aae25120cb8a1e213e1c430a29f
Ian Myjer <imyjer@gmail.com>
2024-10-05 17:01:05 -0700

    add docs

--- setup.sh
+++ setup.sh
@@ -3,8 +3,15 @@ sudo apt-get update && apt-get install -y \
     git \
     vim \
     htop \
-    sl
+    sl \
+    ufw
 
 # install docker
 # https://docs.docker.com/engine/install/debian/
 # https://docs.docker.com/engine/install/linux-postinstall/
+
+# install tailscale
+# setup server: https://tailscale.com/kb/1245/set-up-servers
+# setup firewall: https://tailscale.com/kb/1077/secure-server-ubuntu
+# tailcale up <flags>
+
